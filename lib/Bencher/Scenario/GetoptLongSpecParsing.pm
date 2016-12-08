7716b45928a9381ae0461f7cf2eeb154cbffa00d
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-08 17:25:20 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/GetoptLongSpecParsing.pm
@@ -0,0 +1,28 @@
+package Bencher::Scenario::GetoptLongSpecParsing;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark parsing of Getopt::Long option spec',
+    participants => [
+        {
+            module => 'Getopt::Long::Spec',
+            code_template => 'Getopt::Long::Spec->new->parse(<spec>)',
+        },
+        {
+            fcall_template => 'Getopt::Long::Util::parse_getopt_long_opt_spec(<spec>)',
+        },
+    ],
+    datasets => [
+        {args=>{spec => 'name=s'}},
+        {args=>{spec => 'name|N=s@'}},
+    ],
+};
+
+1;
+# ABSTRACT:
